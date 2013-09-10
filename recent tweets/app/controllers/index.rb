455938bfe64cd69b28bb397003e02184ca67d4c0
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-09 17:45:47 -0700

    add ajax

--- recent tweets/app/controllers/index.rb
+++ recent tweets/app/controllers/index.rb
@@ -1,7 +1,33 @@
 get '/' do
+
+  erb :home
 end
 
-get '/:username' do
+# get '/:username' do
+#   @user = User.find_by_screen_name(params[:username])
+
+#   unless @user
+#     @user = User.new(screen_name: params[:username])
+#     @user.save
+#   end
+
+#   @tweets = @user.fetch_tweets!
+#   puts @tweets
+
+#   erb :tweets
+
+#   # 10.times do |i|
+#   #   puts "#{t[i][:text]}"
+#   # end
+
+#   # Twitter.search("ycombinator", :count => 10, :result_type => "recent").results.map do |status|
+#   # "#{status.from_user}: #{status.text} \n"
+#   # end
+
+# end
+
+post '/username' do
+unless params[:username].empty?
   @user = User.find_by_screen_name(params[:username])
 
   unless @user
@@ -9,18 +35,11 @@ get '/:username' do
     @user.save
   end
 
-  @tweets = @user.fetch_tweets!
-  puts @tweets
-
-  erb :tweets
-
-  # 10.times do |i|
-  #   puts "#{t[i][:text]}"
+  # if @user.tweets_stale?
+  #   erb :beachball, layout: false
+  # else
+    @tweets = @user.fetch_tweets!
+    erb :tweets, layout: false
   # end
-
-  # Twitter.search("ycombinator", :count => 10, :result_type => "recent").results.map do |status|
-  # "#{status.from_user}: #{status.text} \n"
-  # end
-
 end
-
+end
--- recent tweets/public/js/application.js
+++ recent tweets/public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    $('p').append("<img src='beachball.gif' id='beachball'>")
+    $.ajax({
+      url: this.action,
+      type: this.method,
+      data: $(this).serialize()
+    }).done(function(server_data){
+      $('#beachball').remove();
+      $('#ten_tweets').remove();
+      $('form').append(server_data)
+    }).fail(function(server_data){
+      console.log("fail")
+    })
+  })
 });
